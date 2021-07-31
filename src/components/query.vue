9e85daae23e5e433201bda137c94e15af2e7b77f
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-07-31 16:59:38 -0700

    Start query fields form

--- src/components/query.vue
+++ src/components/query.vue
@@ -1,44 +1,182 @@
 <template>
   <div id="query">
     <topBar v-on:homeWanted="homeWanted" />
-    <center>
-        <input type="text" placeholder="Revenue USD Mil" size="30">
-        <input type="text" placeholder="Gross Margin %" size="30">
-        <input type="text" placeholder="Operating Income USD Mil" size="30">
-        <input type="text" placeholder="Operating Margin %" size="30">
-        <input type="text" placeholder="Net Income USD Mil" size="30">
-        <input type="text" placeholder="Earnings Per Share USD" size="30">
-        <input type="text" placeholder="Dividents USD" size="30">
-        <input type="text" placeholder="Payout Ratio %" size="30">
-        <input type="text" placeholder="Shares Mil" size="30">
-        <input type="text" placeholder="Book Value Per Share * USD" size="30">
-        <input type="text" placeholder="Operating Cash Flow USD Mil" size="30">
-        <input type="text" placeholder="Cap Spending USD Mil" size="30">
-        <input type="text" placeholder="Free Cash Flow USD Mil" size="30">
-        <input type="text" placeholder="Free Cash Flow Per Share * USD" size="30">
-        <input type="text" placeholder="Working Capital USD Mil" size="30">
-        <input type="text" placeholder="Margins % of Sales" size="30">
-    </center>
+
+    <!--Have checked-->
+    <input type="checkbox" class="formField" id="Revenue USD Mil" />
+    <label for="Revenue USD Mil" class="form-check-label">Revenue USD Mil</label>
+
+    <input type="checkbox" class="formField" id="Gross Margin %" />
+    <label for="Gross Margin %">Gross Margin %</label>
+
+    <input type="checkbox" class="formField" id="Operating Income USD Mil" />
+    <label for="Operating Income USD Mil">Operating Income USD Mil</label>
+
+    <input type="checkbox" class="formField" id="Operating Margin %" />
+    <label for="Operating Margin %">Operating Margin %</label>
+
+    <input type="checkbox" class="formField" id="Net Income USD Mil" />
+    <label for="Net Income USD Mil">Net Income USD Mil</label>
+    <!--<p />-->
+
+    <input type="checkbox" class="formField" id="Earnings Per Share USD" />
+    <label for="Earnings Per Share USD">Earnings Per Share USD</label>
+    <!--<p />-->
+
+    <input type="checkbox" class="formField" id="Dividents USD" />
+    <label for="Dividents USD">Dividents USD</label>
+    <!--<p />-->
+
+    <input type="checkbox" class="formField" id="Payout Ratio %" />
+    <label for="Payout Ratio %">Payout Ratio %</label>
+    <!--<p />-->
+
+    <input type="checkbox" class="formField" id="Shares Mil" />
+    <label for="Shares Mil">Shares Mil</label>
+    <!--<p />-->
+
+    <input type="checkbox" class="formField" id="Book Value Per Share * USD" />
+    <label for="Book Value Per Share * USD">Book Value Per Share * USD</label>
+    <!--<p />-->
+
+    <input type="checkbox" class="formField" id="Operating Cash Flow USD Mil" />
+    <label for="Operating Cash Flow USD Mil">Operating Cash Flow USD Mil</label>
+    <!--<p />-->
+
+    <input type="checkbox" class="formField" id="Cap Spending USD Mil" />
+    <label for="Cap Spending USD Mil">Cap Spending USD Mil</label>
+    <!--<p />-->
+
+    <input type="checkbox" class="formField" id="Free Cash Flow USD Mil" />
+    <label for="Free Cash Flow USD Mil">Free Cash Flow USD Mil</label>
+    <!--<p />-->
+
+    <input type="checkbox" class="formField" id="Free Cash Flow Per Share * USD"/>
+    <label for="Free Cash Flow Per Share * USD">Free Cash Flow Per Share * USD</label>
+    <!--<p />-->
+
+    <input type="checkbox" class="formField" id="Working Capital USD Mil" />
+    <label for="Working Capital USD Mil">Working Capital USD Mil</label>
+    <!--<p />-->
+
+    <input type="checkbox" class="formField" id="Margins % of Sales" />
+    <label for="Margins % of Sales">Margins % of Sales</label>
+    <!--<p />-->
+
+    <!--submit button-->
+    <script type="application/javascript">
+function createChart(url) {
+  function operateOnData(data) {
+    var fields = [
+      "Revenue USD Mil",
+      "Gross Margin %",
+      "Operating Income USD Mil",
+      "Operating Margin %",
+      "Net Income USD Mil",
+      "Earnings Per Share USD",
+      "Dividents USD",
+      "Payout Ratio %",
+      "Shares Mil",
+      "Book Value Per Share * USD",
+      "Operating Cash Flow USD Mil",
+      "Cap Spending USD Mil",
+      "Free Cash Flow USD Mil",
+      "Free Cash Flow Per Share * USD",
+      "Working Capital USD Mil",
+      "Margins % of Sales",
+    ];
+    var headers = [];
+
+    fields.forEach((item, index) => {
+      if (document.getElementById(item).checked) {
+        headers.push(item);
+      }
+    });
+
+    var data = JSON.parse(data);
+    var parsedData = {};
+
+    for (var i = 0; i < headers.length; i++) {
+      parsedData[headers[i]] = {};
+    }
+
+    var company_list = [];
+
+    for (comp in data) {
+      for (var i = 0; i < data[comp][1].length; i++) {
+        var valList = [];
+        for (k in data[comp][1][i]) {
+          valList.push(data[comp][1][i][k] * 100);
+        }
+        parsedData[headers[i]][comp] = valList;
+      }
+      company_list.push(comp);
+    }
+    date_list = Object.keys(data[company_list[0]][1][0]);
+    var compTable = document.createElement("ol");
+
+    for (var i in data) {
+      var ol = document.createElement("li");
+      ol.textContent = i + ": " + data[i][0] * 100;
+      compTable.appendChild(ol);
+    }
+    document.body.appendChild(compTable);
+
+    for (var i in parsedData) {
+      var data_sets = [];
+      for (k in parsedData[i]) {
+        var raw_data_set = {
+          label: k,
+          data: parsedData[i][k],
+        };
+        data_sets.push(raw_data_set);
+      }
+      var data = {
+        labels: date_list,
+        datasets: data_sets,
+      };
+      blob = document.createElement("canvas");
+      document.body.appendChild(blob);
+      var ctx = blob.getContext("2d");
+      var chart = new Chart(ctx, {
+        type: "line",
+        data: data,
+        options: {
+          plugins: {
+            title: {
+              display: true,
+              text: i,
+            },
+          },
+        },
+      });
+    }
+  }
+  fetch(url).then((k) =>
+    k.text().then((data) => {
+      operateOnData(data);
+    })
+  );
+}
+    </script>
+    <button onclick="createChart('https://stock-api-site.herokuapp.com/api/similar_companies?company=AAPL&values=Revenue%20USD%20Mil,Gross%20Margin')"></button>
   </div>
-
-
 </template>
 
 <script>
-import topBar from './topBar.vue'
+import topBar from "./topBar.vue";
 
 export default {
-  name: 'query',
+  name: "query",
   components: { topBar },
   methods: {
-    homeWanted (event) {
-      event
-      this.$emit('homeWanted')
+    homeWanted(event) {
+      event;
+      this.$emit("homeWanted");
     }
-  }
-}
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
 </style>
